396943a4901f88587271494ca44d3896e6e3b901
jacanced <jacanced@gmail.com>
2022-10-05 11:51:07 -0400

    added RBAC

--- web/src/components/CommentsCell/CommentsCell.js
+++ web/src/components/CommentsCell/CommentsCell.js
@@ -6,10 +6,29 @@ export const QUERY = gql`
       id
       name
       body
+      postId
       createdAt
     }
   }
 `
+export const standard = () => ({
+  comments: [
+    {
+      id: 1,
+      name: 'Rob Cameron',
+      body: 'First comment',
+      postId: 1,
+      createdAt: '2020-01-02T12:34:56Z',
+    },
+    {
+      id: 2,
+      name: 'David Price',
+      body: 'Second comment',
+      postId: 2,
+      createdAt: '2020-02-03T23:00:00Z',
+    },
+  ],
+})
 
 export const Loading = () => <div>Loading...</div>
 
